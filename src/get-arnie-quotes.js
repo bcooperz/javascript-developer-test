9ff9f7d73f8530fdfca27a67aab0690cdf37b5fe
bcooperz <brandon_cooper1995@hotmail.com>
2025-06-24 21:41:04 +1000

    Implement transformation of HTTP responses in getArnieQuotes function

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,33 @@
-const { httpGet } = require('./mock-http-interface');
+const { httpGet } = require("./mock-http-interface");
 
+/**
+ * Transforms an HTTP response into the required format.
+ * @param {Object} response - The HTTP response object
+ * @returns {Object} The transformed response
+ */
+const transformResponse = (response) => {
+  const body = JSON.parse(response.body);
+
+  if (response.status === 200) {
+    return { "Arnie Quote": body.message };
+  } else {
+    return { FAILURE: body.message };
+  }
+};
+
+/**
+ * Executes a HTTP GET request on each of the URLs, transforms each of the HTTP responses according to the challenge instructions and returns the results.
+ * @param {string[]} urls The urls to be requested
+ * @return {Promise<Object[]>} A promise which resolves to a results array.
+ */
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  const promises = urls.map(async (url) => {
+    // Relies on httpGet to handle errors and return the correct format
+    const response = await httpGet(url);
+    return transformResponse(response);
+  });
+
+  return await Promise.all(promises);
 };
 
 module.exports = {
